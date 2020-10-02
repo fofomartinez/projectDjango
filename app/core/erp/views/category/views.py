d973e22d3e9d0c37a99d46b1b3d1b420989b7c59
rodolfoMartinez <rmartinezr4@miumg.edu.gt>
2020-10-02 01:31:38 -0600

    Segunda subida

---
+++ app/core/erp/views/category/views.py
@@ -0,0 +1,28 @@
+from django.views.generic import ListView
+
+from django.shortcuts import render
+
+from core.erp.models import Category, Product
+
+
+def category_list(request):
+    data = {
+        'title': 'Listado de Categorías',
+        'categories': Category.objects.all()
+    }
+    return render(request, 'category/list.html', data)
+
+
+class CategoryListView(ListView):
+    model = Category
+    template_name = 'category/list.html'
+
+    def get_queryset(self):
+        return Category.objects.all()
+        #return Product.objects.filter(name__startswith='Y')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Listado de categorias'
+        #context['object_list']= Product.objects.all()
+        return context
