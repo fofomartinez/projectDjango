6fadca8aba5d2de4d4796690104237828b5f5d1e
Ariel Peinado <apeinadob@miumg.edu.gt>
2020-10-10 07:39:34 -0600

    Cuarta Subida Vd55

--- app/core/erp/models.py
+++ app/core/erp/models.py
@@ -1,3 +1,4 @@
+from crum import get_current_user
 from django.db import models
 from datetime import datetime
 
@@ -5,17 +6,28 @@ from django.forms import model_to_dict
 
 from config.settings import MEDIA_URL, STATIC_URL
 from core.erp.choices import gender_choices
+from core.models import BaseModel
 
 
-class Category(models.Model):
+class Category(BaseModel):
     name = models.CharField(max_length=150, verbose_name='Nombre', unique=True)
     desc = models.CharField(max_length=500, null=True, blank=True, verbose_name='Descripción')
 
     def __str__(self):
         return self.name
 
+    def save(self, force_insert=False, force_update=False, using=None,
+             update_fields=None):
+        user = get_current_user()
+        if user is not None:
+            if not self.pk:
+                self.user_creation = user
+            else:
+                self.user_updated = user
+        super(Category, self).save()
+
     def toJSON(self):
-        item = model_to_dict(self)
+        item = model_to_dict(self, exclude=['user_creation', 'user_updated'])
         return item
 
     class Meta:
@@ -48,12 +60,18 @@ class Client(models.Model):
     names = models.CharField(max_length=150, verbose_name='Nombres')
     surnames = models.CharField(max_length=150, verbose_name='Apellidos')
     dni = models.CharField(max_length=10, unique=True, verbose_name='Dni')
-    birthday = models.DateField(default=datetime.now, verbose_name='Fecha de nacimiento')
+    date_birthday = models.DateField(default=datetime.now, verbose_name='Fecha de nacimiento')
     address = models.CharField(max_length=150, null=True, blank=True, verbose_name='Dirección')
-    sexo = models.CharField(max_length=10, choices=gender_choices, default='male', verbose_name='Sexo')
+    gender = models.CharField(max_length=10, choices=gender_choices, default='male', verbose_name='Sexo')
 
     def __str__(self):
         return self.names
+
+    def toJSON(self):
+        item = model_to_dict(self)
+        item['gender'] = {'id': self.gender, 'name': self.get_gender_display()}
+        item['date_birthday'] = self.date_birthday.strftime('%Y-%m-%d')
+        return item
 
     class Meta:
         verbose_name = 'Cliente'
